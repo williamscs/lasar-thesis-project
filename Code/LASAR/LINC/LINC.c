9746c99e473c70d9f0d3826f143b6a978c9420f5
cwilliams88@gmail.com <cwilliams88@gmail.com@dc2e58e3-6ae4-9c75-41f9-616670aedc83>
2012-02-01 22:23:45 +0000

    Moved the Schematics and PCB Design items into their own folder and created a new AVR Studio 5 Solution which contains 3 separate projects: Remote, LINC, and Satellite.

---
+++ Code/LASAR/LINC/LINC.c
@@ -0,0 +1,16 @@
+/*
+ * LINC.c
+ *
+ * Created: 2/1/2012 4:20:06 PM
+ *  Author: Dubs
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
