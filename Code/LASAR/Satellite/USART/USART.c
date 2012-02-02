8c3b3d4c0e544c99a9d00b7e855373fe70cbf804
cwilliams88@gmail.com <cwilliams88@gmail.com@dc2e58e3-6ae4-9c75-41f9-616670aedc83>
2012-02-02 21:39:37 +0000

    Commit verification

---
+++ Code/LASAR/Satellite/USART/USART.c
@@ -0,0 +1,35 @@
+/*
+ * USART.c
+ *
+ * Created: 2/2/2012 2:21:34 PM
+ *  Author: Dubs
+ */ 
+
+#include <avr\io.h> 
+
+void USART_Init( unsigned int ubrr)
+{
+/*Set baud rate */
+    UBRR0H = (ubrr >> 8);
+    UBRR0L = ubrr;
+    
+    UCSR0B = (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0);      // Enable receiver and transmitter and interrupt receive
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);    // Set frame: 8data, 1 stp
+}
+
+
+void USART_Transmit( unsigned char data )
+{
+	/* Wait for empty transmit buffer */
+    while ( !(UCSR0A & (1 << UDRE0)) ) // Wait until buffer is empty
+
+	;
+	/* Put data into buffer, sends the data */
+	UDR0 = data;
+}
+
+
+unsigned char USART_Receive( void )
+{
+	
+}
