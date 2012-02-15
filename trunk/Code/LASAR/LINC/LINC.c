2a31f5f046dd034bb9e113af70b03dfe9ecff48a
cwilliams88@gmail.com <cwilliams88@gmail.com@dc2e58e3-6ae4-9c75-41f9-616670aedc83>
2012-02-15 05:39:50 +0000

    Finished porting AC dimming code to AVR C.
    TODO: Verify all things discovered about previous generation board to the new board that we will be ordering.

--- trunk/Code/LASAR/LINC/LINC.c
+++ trunk/Code/LASAR/LINC/LINC.c
@@ -4,9 +4,11 @@
  * Created: 2/1/2012 4:20:06 PM
  *  Author: Dubs
  */ 
+#define F_CPU 16000000
 
 #include <avr/io.h>
 #include "lcd.h"
+
 
 int main(void)
 {
---
+++ trunk/Code/LASAR/Remote/Release/makedep.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+LCD Library\cfaf320.c
+
+LCD Library\Font16.c
+
+LCD Library\Font8.c
+
+LCD Library\GrLCD.c
+
+LCD Library\helvb10.c
+
+LCD Library\helvn6.c
+
+LCD Library\timesb12.c
+
+Remote.c
+
