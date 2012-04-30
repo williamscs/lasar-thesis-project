5e0941fcc788ea1173e95c3be33ba075a65d5e44
cwilliams88@gmail.com <cwilliams88@gmail.com@dc2e58e3-6ae4-9c75-41f9-616670aedc83>
2012-04-30 18:17:19 +0000

    Fixed default state for the command parser.  The function setBlinds now actually moves the blinds, rather than simply set the current blinds value in software.
    Fixed a bug that was introduced which caused the blinds to "double-set", which caused the servo to draw excess current, resetting the system.
    
    Documentation update.

--- Code/Documentation/html/search/all_6c.js
+++ Code/Documentation/html/search/all_6c.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['lasarcontrol',['LASARControl',['../classcom_1_1example_1_1android_1_1_bluetooth_chat_1_1_l_a_s_a_r_control.html',1,'com::example::android::BluetoothChat']]],
-  ['lasarcontrol_2ejava',['LasarControl.java',['../_lasar_control_8java.html',1,'']]],
+  ['lasarcontrol_2ejava',['LASARControl.java',['../_l_a_s_a_r_control_8java.html',1,'']]],
   ['leave',['leave',['../_satellite_8c.html#a44ab544c3240a2ef9c9e450af43920c8',1,'Satellite.c']]]
 ];
